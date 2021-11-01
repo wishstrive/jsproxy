29ae866b427fff6644efc836fb0834137251fa94
wishstrive <75101624+wishstrive@users.noreply.github.com>
2021-11-01 18:38:06 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -46,6 +46,7 @@ jsproxy_config({
         //'b.hehe.workers.dev': 1,
         //'b.lulu.workers.dev': 1,
         //'b.jsproxy.workers.dev': 1,
+        https://w.wishstrive.workers.dev: 1,
       }
     }
   },
